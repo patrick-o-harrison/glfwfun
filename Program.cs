0070cb8ea44f726c52be8684e081343f2b435e49
Patrick Harrison <patrick.o.harrison@gmail.com>
2019-02-15 13:07:43 -0600

    Demo showing off how to link C# to GLFW

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace GLFWFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Glfw.SetErrorCallback((error, desc) => { Console.WriteLine("Wow..");});
+            if( Glfw.Init() != Glfw.True ) {
+                Console.WriteLine("Huh...");
+            }
+
+            var win = Glfw.CreateWindow(400, 400, "Fuzzy Pickles", IntPtr.Zero, IntPtr.Zero);
+
+            while( Glfw.WindowShouldClose(win) == Glfw.False ) {
+                Glfw.PollEvents();
+            }
+        }
+    }
+}
